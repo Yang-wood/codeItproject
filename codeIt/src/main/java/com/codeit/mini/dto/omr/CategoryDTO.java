cb4ac5d5393335f7c79c098243f328de30e0c863
yhb <snoopy3016@gmail.com>
2025-06-12 16:37:01 +0900

    CodeItApplication 에 @EnableJpaAuditing 추가 (한별)

--- codeIt/src/main/java/com/codeit/mini/dto/omr/CategoryDTO.java
+++ codeIt/src/main/java/com/codeit/mini/dto/omr/CategoryDTO.java
@@ -1,5 +1,47 @@
 package com.codeit.mini.dto.omr;
 
-public class CategoryDTO {
 
+import com.codeit.mini.entity.omr.CategoryEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDTO {
+	
+	private Long categoryId;
+	
+	private String categoryType;
+	
+	
+	public CategoryDTO toDto(CategoryEntity categoryEntity) {
+		
+		return CategoryDTO.builder().categoryId(categoryEntity.getCategoryId())
+									.categoryType(categoryEntity.getCategoryType())
+									.build();
+	}
+	
+	
+	public CategoryEntity toEntity(CategoryDTO categoryDTO) {
+		
+		return CategoryEntity.builder().categoryId(categoryDTO.getCategoryId())
+									   .categoryType(categoryDTO.getCategoryType())
+									   .build();
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
+
