b4c139adcfa4c4e65c33081f4c3e7b44210b4245
unp12 <unp12@REX>
2025-06-12 11:22:09 +0900

    상품 - 자판기 엔티티 커밋_양정화

---
+++ codeIt/src/main/java/com/codeit/mini/entity/vending/MachineItemEntity.java
@@ -0,0 +1,25 @@
+package com.codeit.mini.entity.vending;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "machine_item")
+public class MachineItemEntity {
+
+	@EmbeddedId
+	private MachineItemId id;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@MapsId("machineId")
+	@JoinColumn(name =" machine_id")
+	private VendingMachinesEntity vendingMachine;
+	
+	
+	
+}
