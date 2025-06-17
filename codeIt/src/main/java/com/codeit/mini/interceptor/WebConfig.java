53135e27e6c96d1f105fd5a952d639f729a0147a
jihyeon <wlgusl147@naver.com>
2025-06-17 12:37:24 +0900

    복구한 파일들 커밋

--- codeIt/src/main/java/com/codeit/mini/interceptor/WebConfig.java
+++ codeIt/src/main/java/com/codeit/mini/interceptor/WebConfig.java
@@ -9,19 +9,25 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
+	
 	// AuthInterceptor를 전체 웹 애플리케이션에 적용
 	@Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new AuthInterceptor())
                 .addPathPatterns("/member/**") // 보호할 경로
                 .excludePathPatterns(
-                    "/member/login",
-                    "/member/register",
-                    "/member/checkId",
-                    "/member/send-auth-email",       // ✅ 인증코드 전송 제외
-                    "/member/verify-auth-code",      // ✅ 인증코드 검증 제외
-                    "/main",
-                    "/error"
+                		"/member/login",
+                        "/member/register",
+                        "/member/checkId",
+                        "/member/send-auth-email",       // 인증코드 전송 제외
+                        "/member/verify-auth-code",      // 인증코드 검증 제외
+                        "/member/ajax/find-id",        	 // 아이디 찾기 예외 
+                        "/member/ajax/find-pw",
+                        "/member/ajax/update-name",
+                        "/member/ajax/update-email",
+                        "/member//ajax/update-password",
+                        "/main",
+                        "/error"
                 );
     }
 }
